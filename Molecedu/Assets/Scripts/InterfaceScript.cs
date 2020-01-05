830aa2d5dd7cfe63f158a1338eb9db824db6dedc
Jia Xuan Ng <>
2020-01-05 16:27:50 +0000

    Login/Registration
    -Login added
    -Registration added
    -Database read/write (for accounts) added

--- Molecedu/Assets/Scripts/InterfaceScript.cs
+++ Molecedu/Assets/Scripts/InterfaceScript.cs
@@ -6,32 +6,19 @@ public class InterfaceScript : MonoBehaviour
 {
     public void CloseOpenInterface()
     {
-        if (this.GetComponent<CanvasGroup>().alpha == 1f)
+        if (this.GetComponent<CanvasGroup>().alpha == 1)
         {
             this.GetComponent<CanvasGroup>().alpha = 0f;
             this.GetComponent<CanvasGroup>().interactable = false;
             this.GetComponent<CanvasGroup>().blocksRaycasts = false;
         }
-        else
+        else if (this.GetComponent<CanvasGroup>().alpha == 0)
         {
             this.GetComponent<CanvasGroup>().alpha = 1f;
-            this.GetComponent<CanvasGroup>().interactable = true;
+            this.GetComponent<CanvasGroup>().interactable = true;            
             this.GetComponent<CanvasGroup>().blocksRaycasts = true;
         }
+
     }
-    public void CloseOpenInterface(CanvasGroup canvasGroup)
-    {
-        if (canvasGroup.GetComponent<CanvasGroup>().alpha == 1f)
-        {
-            canvasGroup.GetComponent<CanvasGroup>().alpha = 0f;
-            canvasGroup.GetComponent<CanvasGroup>().interactable = false;
-            canvasGroup.GetComponent<CanvasGroup>().blocksRaycasts = false;
-        }
-        else
-        {
-            canvasGroup.GetComponent<CanvasGroup>().alpha = 1f;
-            canvasGroup.GetComponent<CanvasGroup>().interactable = true;
-            canvasGroup.GetComponent<CanvasGroup>().blocksRaycasts = true;
-        }
-    }
+
 }
